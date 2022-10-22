6cc87ecb2932a1ff63c94c0af7fd840e3461f0df
Huy <lenambaohuy107@gmail.com>
2022-10-22 14:25:07 +0700

    first commit

---
+++ homework.js
@@ -0,0 +1,37 @@
+// homework
+
+//input
+var salaryPerDay = 100;
+var workingDay = 25;
+var totalSalary = salaryPerDay * workingDay;
+console.log(totalSalary)
+
+// input
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 5;
+var e = 6;
+var average= (a + b + c + d + e)/5;
+console.log(average)
+
+// input
+var rateOfUsd = 23500;
+var quantityUsd = 100;
+var totalCurrencyExchange = rateOfUsd * quantityUsd;
+console.log(totalCurrencyExchange)
+
+// input
+var widthOfRectangle = 20;
+var heightOfRectangle = 10 ;
+var acreageOfRectangle = widthOfRectangle * heightOfRectangle;
+var perimeterOfRectangle = (widthOfRectangle + heightOfRectangle)/2;
+
+console.log(acreageOfRectangle,"Diện tích hình chữ nhật")
+console.log(perimeterOfRectangle,"Chu vi hình chữ nhật")
+
+
+// input
+var number = 88;
+var totalElementOfNumber = Math.floor(number / 10) + (number % 10);
+console.log(totalElementOfNumber)
---
+++ index.js
@@ -0,0 +1,106 @@
+// console.log("Hello world 2");
+// console.log("hello 2");
+// var age = 24;
+
+// console.log(age);
+
+// var name = "Lê Nam Bảo Huy";
+// name = "Huy Lê"
+// console.log(name);
+
+var age = 20;
+var fullName = "Huy Le";
+var email = "lenambaohuy107@gmail.com";
+var phoneNumber = null;
+var id;     
+
+// toán tử 
+
+// var res1 = 1 + 2;
+// var res2 = 1 - 2;
+// var res3 = 1 * 2;
+// var res4 = 3 / 2;
+// var res5 = 3 % 2;  //lấy phần dư res5 = 1
+
+// res1 +=5;
+// res1++; 
+// res1--;
+// console.log(res1)
+
+// res3 +=4;
+// res3 *=3;
+// res3 /=6;
+// res3 %=2;
+// console.log(res3 );
+
+// var res6 = 10;
+// var res7 = res6++ +2 ; //tính biểu thức + trước rồi mới ++ sau
+// var res7 = (res6++) +2 ; // tính biểu thức trong ngoặc trước rồi + sau
+
+// console.log(res6, res7);
+
+// // a = 5 , b = 6
+// var a = 5 ;
+// var b = 6 ;
+// b = (--b) + (++a)
+
+
+// console.log(a,b,)
+
+
+var a = 3 ;
+var b = 5 ;
+var c = 14.1 ;
+var sum ;
+
+sum = a + b +c ;
+
+
+console.log(a, b, c, sum)
+
+/**
+ * Input:
+ * 
+ * Process:
+ *  - lấy input: (hardcode) 
+ *  - công thức tính = lương 1 ngày * số ngày làm
+ *  - in kết quả
+ *  
+ * 
+ * Output:
+ */
+var workingDays = 20 ;
+var salaryPerDay = 100000 ;
+var totalSalary = workingDays * salaryPerDay ;
+
+console.log("Total:", totalSalary)
+
+
+var a = 1 ;
+var b = 2 ;
+var c = 3 ;
+var d = 4 ;
+var e = 5 ;
+var averageNumber = (a + b + c + d + e)/5 ;
+
+console.log("Average:" ,averageNumber)
+
+var rateUsd = 23500;
+var amountChange = 100;
+var currencyExchange = rateUsd * amountChange;
+
+console.log("Total :" , currencyExchange)
+
+var widthOfRectangle = 100 ;
+var heightOfRectangle = 50 ;
+var acreageOfRectangle = widthOfRectangle *heightOfRectangle ;
+m
+console.log("Acreage:", acreageOfRectangle)
+
+var number = 83 ;
+var num1   = number % 10 ;
+var num2   = Math.floor (number/10);
+
+var sumElementOfNumber = num1 + num2 ;
+
+console.log("Sum:" ,sumElementOfNumber)
